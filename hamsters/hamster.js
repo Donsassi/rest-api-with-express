4daaa1315c6fd30c9f5499291957c9cf6b09d0f8
Tero sassi <terosassi@gmail.com>
2021-04-23 10:34:34 +0200

    DELETE

--- hamsters/hamster.js
+++ hamsters/hamster.js
@@ -1,6 +1,5 @@
 const getDatabase = require('../database.js')
 const db = getDatabase()
-
 const express = require('express')
 const router = express.Router()
 
@@ -23,6 +22,8 @@ router.get('/', async (req, res) => {
     res.send(items)
 })
 
+//Random
+
 
 // ID 
 router.get('/:id', async (req, res) => {
@@ -40,8 +41,8 @@ router.get('/:id', async (req, res) => {
 // POST 
 router.post('/', async (req, res) => {
     const object = req.body
-    if( !object ) {
-        res.sendStatus(400)
+    if( !isHamsterObject(object) ) {
+        res.status(400).send("obj should include 'name, age, favFood, loves, imgName, wins, defeats, games'")
         return
     }
    const docRef = await db.collection('hamsters').add(object)
@@ -49,7 +50,42 @@ router.post('/', async (req, res) => {
 })
 
 // PUT
+router.put('/:id', async (req, res) => { 
+    const object = req.body   
+    const id = req.params.id
+    
+    if ( !object || !id ) {
+        res.sendStatus(400)
+        return
+    }
+
+    const docRef = db.collection('hamsters').doc(id)
+    await docRef.set(object, { merge: true })
+    res.sendStatus(200)
+
+})
 
 
+function isHamsterObject(maybeObject) {
+    if( !maybeObject )
+         return false
+    else if (  !maybeObject.name || !maybeObject.age | !maybeObject.favFood || !maybeObject.loves || !maybeObject.imgName || !maybeObject.wins || !maybeObject.games )
+         return false
+
+    return true
+}
+
+//Delete
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const docRef = await db.collection('hamsters').doc(id).get()
+
+    if( !docRef.exists ) {
+        res.sendStatus(400)
+        return
+    }
+       await db.collection('hamsters').doc(id).delete()
+       res.sendStatus(200)
+})
 
 module.exports = router
