8468012c3802ffa49ce727527dfde9f9666e61c4
Tero sassi <terosassi@gmail.com>
2021-04-26 10:22:22 +0200

    fixat key

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 let serviceAccount;
-if ( process.env.PRIVATE_KEY ) { 
-    const serviceAccount = JSON.parse(process.env.PRIVATE_KEY) 
- } else {
-const serviceAccount = require("./fbpk.json");
+if( process.env.PRIVATE_KEY ) {
+
+	serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
+} else {
+
+	serviceAccount = require("./firebase-private-key.json");
 }
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
